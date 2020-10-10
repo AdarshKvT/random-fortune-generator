2564c00e8b5e0d54dffc7a92be85db9497cbe2db
aadarsh_kvt <48561692+AdarshKvT@users.noreply.github.com>
2020-10-10 19:54:10 +0530

    Create RandomFortune.java

---
+++ RandomFortune.java
@@ -0,0 +1,22 @@
+package com.kvtsoft.demoapp;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomFortune {
+
+	public static void main(String[] args) {
+
+		String[] fortuneArray = { "Character consist of what you do on the third and forth tries",
+				"Winner never quite and quitter never win", "You can't beat the person who won't give up" };
+
+		// int arrayLeng = fortuneArray.length;
+		// System.out.println("arrayLeng: " + arrayLeng);
+
+		Random random = new Random();
+		int fortuneNum = random.nextInt(fortuneArray.length);
+		System.out.println(fortuneArray[fortuneNum]);
+
+	}
+
+}
